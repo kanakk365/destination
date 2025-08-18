332464c33d1337932a04a97ff8b50294f21da81a
Kanak <kanakkumarmahala000@gmail.com>
2025-08-18 13:59:28 +0530

    fix : added new vid

--- components/hero.tsx
+++ components/hero.tsx
@@ -72,7 +72,7 @@ const Hero = () => {
                 muted
                 playsInline
               >
-                <source src="/hero.mp4" type="video/mp4" />
+                <source src="/vid/newHero.mp4" type="video/mp4" />
                 Your browser does not support the video tag.
               </video>
             </div>
